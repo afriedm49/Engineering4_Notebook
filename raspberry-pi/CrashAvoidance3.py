50722777ef2af77f4ee3348e09645a1a27dbee18
afriedm49 <afriedm49@charlottesvilleschools.org>
2022-09-27 13:40:24 -0400

    crash3

--- raspberry-pi/CrashAvoidance3.py
+++ raspberry-pi/CrashAvoidance3.py
@@ -7,19 +7,28 @@ import adafruit_mpu6050
 import displayio
 import terminalio
 import time
+displayio.release_displays()
+
 led = digitalio.DigitalInOut(board.GP2)
 led.direction=digitalio.Direction.OUTPUT
-sda_pin = board.GP14
-scl_pin = board.GP15
+sda_pin = board.GP12
+scl_pin = board.GP13
 i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c, address=0x68)
-displayio.release_displays()
 display_bus = displayio.I2CDisplay(i2c, device_address=0x3d, reset=board.GP16)
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
-
-splash = displayio.Group()
-title = "ANGULAR VELOCITY"
-text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=5)
-splash.append(text_area)
-
-display.show(splash)
+while True:
+    splash = displayio.Group()
+    title = "ANGULAR VELOCITY"
+    xvelocity = "X-velocity: " + str(round(mpu.gyro[0], 3))
+    yvelocity = "Y-velocity: " + str(round(mpu.gyro[1], 3))
+    zvelocity = "Z-velocity: " + str(round(mpu.gyro[2], 3))
+    text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=5)
+    splash.append(text_area)
+    xlabel = label.Label(terminalio.FONT, text= xvelocity, color=0xFFFF00, x=5 , y=20)
+    splash.append(xlabel)
+    ylabel = label.Label(terminalio.FONT, text= yvelocity, color=0xFFFF00, x=5 , y=35)
+    splash.append(ylabel)
+    zlabel = label.Label(terminalio.FONT, text= zvelocity, color=0xFFFF00, x=5 , y=50)
+    splash.append(zlabel)
+    display.show(splash)
