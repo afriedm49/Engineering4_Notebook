98d70c64c9d165a7eb734a612a845e96ee61996a
afriedm49 <afriedm49@charlottesvilleschools.org>
2022-10-04 13:35:55 -0400

    Works now

--- raspberry-pi/LandingArea2.py
+++ raspberry-pi/LandingArea2.py
@@ -39,19 +39,17 @@ while True:
     centerpoint = Circle(64, 32, 3, outline=0xFFFF00)
     splash.append(centerpoint)
 
-    display.show(splash)
-
     try:
         [xcoor1, ycoor1] = input("Enter coordinate 1: ").split(",")
         [xcoor2, ycoor2] = input("Enter coordinate 2: ").split(",")
-        [xcoor3, ycoor3] = input("Enter coordinate 2: ").split(",")
+        [xcoor3, ycoor3] = input("Enter coordinate 3: ").split(",")
         print("The area of the triangle with points (" + xcoor1 + ", " + ycoor1 + ")," + " (" + xcoor2 + ", " + ycoor2 + ")," + " (" + xcoor3 + ", " + ycoor3 + ") is " + str(triArea(xcoor1, ycoor1, xcoor2, ycoor2, xcoor3, ycoor3)))
-        xcoor1 = int(xcoor1)
-        xcoor2 = int(xcoor2)
-        xcoor3 = int(xcoor3)
-        ycoor1 = int(xcoor1)
-        ycoor2 = int(xcoor2)
-        ycoor3 = int(xcoor3)
+        xcoor1 = int(xcoor1) + 64
+        xcoor2 = int(xcoor2) + 64
+        xcoor3 = int(xcoor3) + 64
+        ycoor1 = -int(ycoor1) + 32
+        ycoor2 = -int(ycoor2) + 32
+        ycoor3 = -int(ycoor3) + 32
 
     except:
         print("Error")
