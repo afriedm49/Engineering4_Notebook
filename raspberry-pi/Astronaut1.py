8f15ccae56e28f59491bfabbc1b0accdfb8acf5a
afriedm49 <55894515+afriedm49@users.noreply.github.com>
2022-10-25 13:03:12 -0400

    Update Astronaut1.py

--- raspberry-pi/Astronaut1.py
+++ raspberry-pi/Astronaut1.py
@@ -1,13 +1,13 @@
-import board
+import board  
 import digitalio
 import time
-led = digitalio.DigitalInOut(board.LED)
-led.direction=digitalio.Direction.OUTPUT
-for i in range(10, 0, -1):
+led = digitalio.DigitalInOut(board.LED)     # sets up LED in these 2 lines
+led.direction=digitalio.Direction.OUTPUT    
+for i in range(10, 0, -1):      # for loop to print numbers 10 through 0
     print(i)
     led.value = True
     time.sleep(.5)
     led.value = False
     time.sleep(.5)
 print("Liftoff!")
-led.value = True
+led.value = True        #leaves LED on after the loop ends
