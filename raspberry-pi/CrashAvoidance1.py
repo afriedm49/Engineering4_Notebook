9412e4baa8cebee34019a1a255245b51740f437e
afriedm49 <55894515+afriedm49@users.noreply.github.com>
2022-10-25 13:24:17 -0400

    Update CrashAvoidance1.py

--- raspberry-pi/CrashAvoidance1.py
+++ raspberry-pi/CrashAvoidance1.py
@@ -5,8 +5,8 @@ import adafruit_mpu6050
 import time
 sda_pin = board.GP14
 scl_pin = board.GP15
-i2c = busio.I2C(scl_pin, sda_pin)
-mpu = adafruit_mpu6050.MPU6050(i2c)
+i2c = busio.I2C(scl_pin, sda_pin)       #these two lines set up
+mpu = adafruit_mpu6050.MPU6050(i2c)     # the mpu variable represents the accelerator that has measurements prtinted
 while True:
     print(mpu.acceleration)
     time.sleep(1)
