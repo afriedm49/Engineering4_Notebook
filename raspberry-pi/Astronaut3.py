bf5fc8a78ad549ae1d5aacae86241214ad004372
afriedm49 <55894515+afriedm49@users.noreply.github.com>
2022-10-25 13:10:07 -0400

    Update Astronaut3.py

--- raspberry-pi/Astronaut3.py
+++ raspberry-pi/Astronaut3.py
@@ -6,8 +6,8 @@ blinkled = digitalio.DigitalInOut(board.GP19)
 blinkled.direction=digitalio.Direction.OUTPUT
 staticled = digitalio.DigitalInOut(board.GP15)
 staticled.direction=digitalio.Direction.OUTPUT
-button = digitalio.DigitalInOut(board.GP13)
-button.pull = digitalio.Pull.DOWN
+button = digitalio.DigitalInOut(board.GP13)     #sets up button to be pushed and start the countdown
+button.pull = digitalio.Pull.DOWN               #button is wired to 3v, so when button is pushed, Hi impendence values pull Down the button for the circuit to close.
 while True:
   if button.value == True:
     for i in range(10, 0, -1):
