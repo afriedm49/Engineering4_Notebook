ae9336d761b76af0e65dc567229cf09e749f0923
afriedm49 <55894515+afriedm49@users.noreply.github.com>
2022-10-25 13:04:10 -0400

    Update Astronaut2.py

--- raspberry-pi/Astronaut2.py
+++ raspberry-pi/Astronaut2.py
@@ -1,9 +1,9 @@
 import board
 import digitalio
 import time
-blinkled = digitalio.DigitalInOut(board.GP1)
+blinkled = digitalio.DigitalInOut(board.GP1)        #blink LED is the red led that blinks
 blinkled.direction=digitalio.Direction.OUTPUT
-staticled = digitalio.DigitalInOut(board.GP8)
+staticled = digitalio.DigitalInOut(board.GP8)       #static LED is the green led that stays on 
 staticled.direction=digitalio.Direction.OUTPUT
 for i in range(10, 0, -1):
     print(i)
