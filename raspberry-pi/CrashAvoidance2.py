4ef9bb58b3f909b10541db1ed0159089669b314d
afriedm49 <55894515+afriedm49@users.noreply.github.com>
2022-10-25 13:26:44 -0400

    Update CrashAvoidance2.py

--- raspberry-pi/CrashAvoidance2.py
+++ raspberry-pi/CrashAvoidance2.py
@@ -10,8 +10,8 @@ scl_pin = board.GP15
 i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c)
 while True:
-    print(mpu.acceleration)
-    if (-1 < mpu.acceleration[2] < 1):
-        led.value = True
+    print(mpu.acceleration)         
+    if (-1 < mpu.acceleration[2] < 1):      #checks if z acceleration is close to 0
+        led.value = True                    #turns led on
     else:
         led.value = False
